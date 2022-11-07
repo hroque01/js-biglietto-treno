ebde723056a7665e300e8386c7bdf993eb0e5660
hroque01 <hadson.roque01@gmail.com>
2022-11-07 15:19:01 +0100

    Eta utente

---
+++ main.js
@@ -0,0 +1,18 @@
+// Chiedere all'utente il numero di chilometri che vuole percorrere e l'eta' del passeggero
+
+// Chidere l'eta dell'Utente
+let etaUtente = parseInt(prompt("Quanti anni hai?"));
+
+console.log("etaUtente");
+
+    // Se l'utente e' minorenne ha lo sconto del 20%
+
+    // ALTRIMENTI se ha piu di 65 anni ha lo sconto del 40%
+    // ALTRIMENTI se non rientra in nessuna di queste categorie rientra nel prezzo pieno
+
+// 0.21 € al km
+const tariffaAlKm = 0.21
+
+
+// output
+    // Prezzo calcolato
